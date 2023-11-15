8ff817994d4a87cddceb6fc31f1a71639dd78154
高炀辉 <insistgyh@foxmail.com>
2023-11-15 10:45:05 +0800

    添加Token类

---
+++ java/com/craftinginterpreters/glox/Token.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.glox;
+
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line; 
+  // 初始化方法
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
